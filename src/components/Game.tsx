025bc400bf665ef0926b719ab3f5ef7c7ce87201
Abhinav <59060016+M3g4s@users.noreply.github.com>
2024-03-28 20:58:51 +0530

    added game rules

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -211,28 +211,69 @@ const Game: React.FC = () => {
 
   return (
     <div>
+      <h1 style={{ textAlign: 'center' }}>
+          BlackJack Game
+        </h1>
+    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+    <div style={{ width: '30%' }}>
       <div>
-        <button onClick={startGame}>Start Game</button>
-        <button onClick={hit}>Hit</button>
-        <button onClick={stand}>Stand</button>
+        <div>
+          <button onClick={startGame}>Start Game</button>
+          <button onClick={hit}>Hit</button>
+          <button onClick={stand}>Stand</button>
+        </div>
+        <p>Wallet $({walletAmount})</p>
+        <input onChange={handleInputChange} />
+        <button onClick={() => placeBet(inputValue)}>place bet</button>
+        <p>Your Hand ({playerTotal})</p>
+        <div>
+          {playerHand.map((card, index) => (
+            <img key={index} src={card.image} alt={`${card.value} of ${card.suit}`} style={{ width: '100px', marginRight: '5px' }} />
+          ))}
+        </div>
+        <p>House Hand ({houseTotal})</p>
+        <div>
+          {houseHand.map((card, index) => (
+            <img key={index} src={card.image} alt={`${card.value} of ${card.suit}`} style={{ width: '100px', marginRight: '5px' }} />
+          ))}
+        </div>
+        {gameOver && <p>{message}</p>}
       </div>
-      <p>Wallet $({walletAmount})</p>
-      <input onChange={handleInputChange} /> 
-      <button onClick={() => placeBet(inputValue)}>place bet</button>
-      <p>Your Hand ({playerTotal})</p>
-      <div>
-        {playerHand.map((card, index) => (
-          <img key={index} src={card.image} alt={`${card.value} of ${card.suit}`} style={{ width: '100px', marginRight: '5px' }} />
-        ))}
-      </div>
-      <p>House Hand ({houseTotal})</p>
-      <div>
-        {houseHand.map((card, index) => (
-          <img key={index} src={card.image} alt={`${card.value} of ${card.suit}`} style={{ width: '100px', marginRight: '5px' }} />
-        ))}
-      </div>
-      {gameOver && <p>{message}</p>}
     </div>
+    <div style={{ width: '60%' }}>
+  <h1 style={{ textAlign: 'center' }}>Rules of the Game</h1>
+  <p>If you’re not familiar with the game, here are the simplified rules we will be going by for this project:</p>
+  <p>The game consists of two players: You vs The House (the computer), where the goal is to beat the House’s hand, without going over 21.</p>
+  <p>A card contains a “point” value equivalent to its number (the 3 of club is worth 3 points...the 9 of spades is worth 9 points...etc etc). Face cards (Jack, Queen, King) are worth TEN points, and the Ace card is either worth 1 or 11, whichever is most helpful for the player’s hand. For example:</p>
+  <p>If the player has a Jack and a Queen, and then draws an Ace, the Ace will be worth 1 point to add up to 21</p>
+  <p>If the player has a Queen and an Ace, the Ace will be worth 11 points to add up to 21</p>
+  <p>If the player has a 2 and an Ace, the Ace will be worth 11 points to get closer to 21</p>
+  <p>If the player has a 2 and a 5, and then draws an Ace, the Ace will be worth 11 points to get closer to 21. If the player then draws a 10, the Ace will now be worth 1 point</p>
+  <p>The House is initially dealt TWO face up cards and no more! This isn’t part of the regular rules for Blackjack, but it is for us. In other words, the House will always only have 2 cards.</p>
+  <p>You are also initially dealt two face up cards, but you have one of the following options:</p>
+  <p>Hit: You are dealt one more card to add to your point value. For this project, the player may hit as many times as they like, until their card value exceeds 21, at which point the game ends in an automatic loss</p>
+  <p>Stand: Ends the round (for the purposes of this project, this will end the game)</p>
+  <p>Once you end the round, the game is over, and there should be a display of whether you won or lost.</p>
+  <p>Don’t deal with a new deck every hand or rely on a refresh. You should be able to run through the deck and shuffle at the end when needed.</p>
+  
+  <h1 style={{ textAlign: 'center' }}>Conditions</h1>
+  <p>You Win if:</p>
+  <ul>
+    <li>The House’s total is 21 and your total is less than 21 (for the purposes of this project, you can ignore this condition, since the House will only have two cards and cannot get a total > 21)</li>
+    <li>Your current total is less than 21 but higher than the House’s total</li>
+    <li>Your current total is 21 and the House’s total is not 21</li>
+  </ul>
+  <p>You lose if:</p>
+  <ul>
+    <li>Your current total totals over 21 (don’t forget to factor in the different edge cases of the Ace card!)</li>
+    <li>You current total is less than 21 but lower than the House’s total</li>
+    <li>You tie with the House</li>
+  </ul>
+</div>
+
+  </div>
+  </div>
+  
   );
         }
 
